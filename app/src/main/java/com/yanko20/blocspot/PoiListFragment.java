400a65f28098124e6089d2ee8dc166a431976b8c
Yan Komizorov <yanko20@gmail.com>
2017-01-10 16:34:04 -0500

    Add poi list attempt

--- app/src/main/java/com/yanko20/blocspot/PoiListFragment.java
+++ app/src/main/java/com/yanko20/blocspot/PoiListFragment.java
@@ -3,25 +3,37 @@ package com.yanko20.blocspot;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PoiListFragment extends Fragment {
+
+public class PoiListFragment extends ListFragment {
 
     public PoiListFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), R.layout.poi_list_item);
+        setListAdapter(adapter);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false);
     }
 
 }
+
+
