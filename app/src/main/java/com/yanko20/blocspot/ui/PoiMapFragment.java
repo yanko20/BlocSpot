6f3e1de96656b3976a643d5053c9c4d782ab2f9b
Yan Komizorov <yanko20@gmail.com>
2017-01-16 20:39:36 -0500

    Added basic map

--- app/src/main/java/com/yanko20/blocspot/ui/PoiMapFragment.java
+++ app/src/main/java/com/yanko20/blocspot/ui/PoiMapFragment.java
@@ -6,15 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.yanko20.blocspot.R;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PoiMapFragment extends Fragment {
+public class PoiMapFragment extends Fragment implements OnMapReadyCallback{
 
+    MapView mapView;
+    GoogleMap map;
 
     public PoiMapFragment() {
         // Required empty public constructor
@@ -24,8 +28,18 @@ public class PoiMapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = (MapView)view.findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
+
+        return view;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+    }
 }
