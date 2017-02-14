74714233005d0a6ab0e79c9664b448168c76ce94
Yan Komizorov <yanko20@gmail.com>
2017-02-13 23:14:04 -0500

    Implement GoogleApiClient interfaces

--- app/src/main/java/com/yanko20/blocspot/ui/MainActivity.java
+++ app/src/main/java/com/yanko20/blocspot/ui/MainActivity.java
@@ -1,22 +1,25 @@
 package com.yanko20.blocspot.ui;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.yanko20.blocspot.R;
 import com.yanko20.blocspot.adapters.PoiFragmentPageAdapter;
-import com.yanko20.blocspot.database.Database;
-import com.yanko20.blocspot.model.PointOfInterest;
 
-import java.util.UUID;
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
-import io.realm.Realm;
-
-public class MainActivity extends AppCompatActivity {
-
-
+    private GoogleApiClient googleApiClient;
+    private final String TAG = "MainActivity.class";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +29,51 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(new PoiFragmentPageAdapter(getSupportFragmentManager()));
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
+        createGoogleApi();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
+
+    private void createGoogleApi(){
+        Log.v(TAG, "createGoogleApi()");
+        if (googleApiClient == null){
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.v(TAG, "GoogleApiClient onConnected()");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.v(TAG, "GoogleApiClient onConnectionSuspended()");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.v(TAG, "GoogleApiClient onConnectionFailed()");
+    }
+
+
 }
