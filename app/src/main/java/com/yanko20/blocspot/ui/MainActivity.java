40789fed3554610ca47f3d8189f91d30d5b40a72
Yan Komizorov <yanko20@gmail.com>
2017-02-18 16:51:46 -0500

    Add current location

--- app/src/main/java/com/yanko20/blocspot/ui/MainActivity.java
+++ app/src/main/java/com/yanko20/blocspot/ui/MainActivity.java
@@ -1,28 +1,42 @@
 package com.yanko20.blocspot.ui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.yanko20.blocspot.R;
 import com.yanko20.blocspot.adapters.PoiFragmentPageAdapter;
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener{
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleApiClient googleApiClient;
+    private Location lastLocation;
+    private LocationRequest locationRequest;
+    private final int UPDATE_INTERVAL = 3 * 60 * 1000; // 3 minutes
+    private final int FASTEST_INTERVAL = 30 * 1000; // 30 seconds
+    private final int REQ_PERMISSION = 1;
     private final String TAG = "MainActivity.class";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
@@ -49,9 +63,9 @@ public class MainActivity extends AppCompatActivity implements
         super.onDestroy();
     }
 
-    private void createGoogleApi(){
-        Log.v(TAG, "createGoogleApi()");
-        if (googleApiClient == null){
+    private void createGoogleApi() {
+        Log.d(TAG, "createGoogleApi()");
+        if (googleApiClient == null) {
             googleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
@@ -62,18 +76,83 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        Log.v(TAG, "GoogleApiClient onConnected()");
+        Log.d(TAG, "GoogleApiClient onConnected()");
+        getLastKnownLocation();
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-        Log.v(TAG, "GoogleApiClient onConnectionSuspended()");
+        Log.d(TAG, "GoogleApiClient onConnectionSuspended()");
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.v(TAG, "GoogleApiClient onConnectionFailed()");
+        Log.d(TAG, "GoogleApiClient onConnectionFailed()");
+    }
+
+    private boolean checkPermission() {
+        Log.d(TAG, "checkPermission()");
+        boolean isPermissionGranted =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED;
+        return isPermissionGranted;
+    }
+
+    private void askPermission() {
+        Log.d(TAG, "askPermission()");
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQ_PERMISSION);
+    }
+
+    private void getLastKnownLocation() {
+        Log.d(TAG, "getLastKnownLocation()");
+        if (checkPermission()) {
+            lastLocation =
+                    LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if (lastLocation != null) {
+                Log.i(TAG, "LastKnown location. " +
+                        "Long: " + lastLocation.getLongitude() +
+                        " | Lat: " + lastLocation.getLatitude());
+            } else {
+                Log.w(TAG, "No location retrieved yet");
+            }
+            startLocationUpdates();
+        } else askPermission();
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQ_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLastKnownLocation();
+                } else {
+                    permissionDenied();
+                }
+            }
+        }
+    }
+
+    private void permissionDenied(){
+        Log.d(TAG, "permissionDenied()");
+    }
+
+    private void startLocationUpdates() {
+        Log.d(TAG, "startLocationUpdates()");
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+        if (checkPermission()) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged()");
+        lastLocation = location;
+    }
 }
