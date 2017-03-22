79c59aac44fd233096d4ed15239952bf8e1d4611
Yan Komizorov <yanko20@gmail.com>
2017-03-22 14:13:39 -0400

    Add to do list

---
+++ app/src/main/java/com/yanko20/blocspot/ToDo.java
@@ -0,0 +1,13 @@
+package com.yanko20.blocspot;
+
+/**
+ * Created by ykomizor on 3/22/2017.
+ */
+
+public class ToDo {
+
+    // todo create a database class for categories
+    // todo build dialog fragment for categories
+    // todo add action bar and conect ot dialog fragment
+
+}
