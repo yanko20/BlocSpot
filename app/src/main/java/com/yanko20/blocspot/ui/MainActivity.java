9aa0b1e1e69cf480913a9c49304ffb4813fca9bb
Yan Komizorov <yanko20@gmail.com>
2017-03-22 16:12:28 -0400

    Added toolbar(actionbar) to main activity

--- app/src/main/java/com/yanko20/blocspot/ui/MainActivity.java
+++ app/src/main/java/com/yanko20/blocspot/ui/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.yanko20.blocspot.BlocSpotApp;
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         Log.v(BlocSpotApp.TAG, "MainActivity.onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
         viewPager.setAdapter(new PoiFragmentPageAdapter(getSupportFragmentManager()));
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
