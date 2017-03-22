43fa364172d9e012015363ab846a448b3dcbf660
Yan Komizorov <yanko20@gmail.com>
2017-03-22 17:33:12 -0400

    Added filter button to toolbar

--- app/src/main/java/com/yanko20/blocspot/ui/MainActivity.java
+++ app/src/main/java/com/yanko20/blocspot/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 
 import com.yanko20.blocspot.BlocSpotApp;
 import com.yanko20.blocspot.R;
@@ -26,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(new PoiFragmentPageAdapter(getSupportFragmentManager()));
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     public static Intent makeNotificationIntent(Context geofenceService, String msg){
