d9668271d1a36bd162a9086bac0990bdab794769
Yan Komizorov <yanko20@gmail.com>
2017-03-22 14:28:21 -0400

    Created Category realm object

--- app/src/main/java/com/yanko20/blocspot/model/Category.java
+++ app/src/main/java/com/yanko20/blocspot/model/Category.java
@@ -1,8 +1,34 @@
 package com.yanko20.blocspot.model;
 
+import io.realm.RealmModel;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.RealmClass;
+
 /**
  * Created by ykomizor on 3/22/2017.
  */
 
-public class Category {
+
+@RealmClass
+public class Category implements RealmModel{
+
+    @PrimaryKey
+    private String name;
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
